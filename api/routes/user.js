acf01696d238f6360e375a0a4d21759068592bac
ngdkhuong <nguyenkhuong31072002@gmail.com>
2023-02-10 23:38:18 +0700

    upload e-commerce app

---
+++ api/routes/user.js
@@ -0,0 +1,48 @@
+const { verifyToken, authorization, isAdmin } = require('./verifyToken');
+const User = require('../modules/User');
+
+const router = require('express').Router();
+
+// UPDATE
+router.put('/:id', authorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true },
+        );
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.json(500).json(err);
+    }
+});
+
+// DELETE
+router.delete('/:id', authorization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json('User has been deleted...');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET USER
+router.get('/find/:id', isAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
