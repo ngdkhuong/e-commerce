7e82f1586e06c1cf3a6e84650c8a5f367e4c0ca4
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-04-26 15:58:38 +0700

    init: db schema

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -4,19 +4,81 @@ const { Schema, model } = mongoose;
 
 const productSchema = new Schema(
     {
-        productName: String,
-        brandName: String,
-        category: String,
-        productImage: [],
-        description: String,
-        price: Number,
-        sellingPrice: Number,
+        name: {
+            type: String,
+            required: [true, 'Please Enter product Name'],
+            trim: true,
+        },
+        description: {
+            type: String,
+            required: [true, 'Please Enter product Description'],
+        },
+        price: {
+            type: Number,
+            required: [true, 'Please Enter product Price'],
+            maxLength: [8, 'Price cannot exceed 8 characters'],
+        },
+        ratings: {
+            type: Number,
+            default: 0,
+        },
+        images: [
+            {
+                public_id: {
+                    type: String,
+                    required: true,
+                },
+                url: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+        category: {
+            type: String,
+            required: [true, 'Please Enter Product Category'],
+        },
+        stock: {
+            type: Number,
+            required: [true, 'Please Enter product Stock'],
+            maxLength: [4, 'Stock cannot exceed 4 characters'],
+            default: 1,
+        },
+        numOfReviews: {
+            type: Number,
+            default: 0,
+        },
+        reviews: [
+            {
+                user: {
+                    type: Schema.ObjectId,
+                    ref: 'User',
+                    required: true,
+                },
+                name: {
+                    type: String,
+                    required: true,
+                },
+                rating: {
+                    type: Number,
+                    required: true,
+                },
+                comment: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+
+        user: {
+            type: Schema.ObjectId,
+            ref: 'User',
+            required: true,
+        },
     },
     {
         timestamps: true,
     },
 );
 
-const productModel = model('product', productSchema);
-
-module.exports = productModel;
+module.exports = model('Product', productSchema);
