825e8abdbb3ff362ec1b8087d7b9dd21f29a07da
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-04-27 14:05:45 +0700

    change: react-app to react-vite

--- server/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDatabase = () => {
-    mongoose.connect(process.env.MONGODB_URI).then((data) => {
-        console.log(`Mongodb connected with server: ${data.connection.host}`);
-    });
-};
-
-module.exports = connectDatabase;
