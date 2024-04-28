252753dc20d082ef551223f026e8f157c9e94fd7
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-04-28 22:35:54 +0700

    push home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { onAuthStateChanged } from 'firebase/auth';
+import { Suspense, lazy, useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Header from './components/header';
+import Loader from './components/loader';
+import ProtectedRoute from './components/protected-route';
+import { auth } from './firebase';
+import { getUser } from './redux/api/userAPI';
+import { userExist, userNotExist } from './redux/reducer/userReducer';
+import { RootState } from './redux/store';
 
-function App() {
-  const [count, setCount] = useState(0)
+const Home = lazy(() => import('./pages/home'));
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+    const { user, loading } = useSelector((state: RootState) => state.userReducer);
 
-export default App
+    return;
+    loading ? (
+        <Loader />
+    ) : (
+        <Router>
+            <Header user={user} />
+            <Suspense fallback={<Loader />}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Suspense>
+            <Toaster position="bottom-center" />
+        </Router>
+    );
+};
+
+export default App;
